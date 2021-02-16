44834956ae70af62df11bd6bf2ae71af4ff7c9a1
Fajar AM <somabangsa@gmail.com>
2021-02-16 10:21:49 +0700

    Update UI

--- src/component/pages/login/Login.tsx
+++ src/component/pages/login/Login.tsx
@@ -48,23 +48,25 @@ class Login extends BaseComponent {
             <div id="LoginForm" className="login-wrapper" style={{ margin: 0, padding: 0 }}> 
                 <div className="text-center" style={{ marginTop: '25px' }}>
                     <svg className="x-app-icon " width="200" height="200">
-                        <circle className="path-circle" fill="white" cx="100" cy="100" r="95" strokeWidth={3} stroke="black" />
+                        <circle className="path-circle" fill="white" cx="100" cy="100" r="95" strokeWidth={3} stroke="rgb(9,26,78)" />
                         {/* <path d="M 50 50 h 100 v 100 h -100 Z" fill="transparent" stroke="black"/> */}
                         <path className="path" d="M 55 145 L 70 145 C 90 120, 110 115 140 102
                             M 140 102 L 145 83 Q 125 85, 100 100 
                             Q 80 98, 70 90 Q 68 85 72 80 Q 100 70, 125 73
                             Q 86 35, 57 80 Q 48 110, 77 118
                             Q 63 130 ,55 145
-                            " strokeWidth={3} stroke="black" fill="yellow" />
+                            " strokeWidth={3} stroke="rgb(9,26,78)" fill="transparent" />
 
                     </svg>
                 </div>
                 <form name='login' onSubmit={(e) => { this.login(e) }}
-                    method='POST' className="form-signin">
+                    method='POST' className="form-signin text-center">
                     <UsernameField value={this.state.username} onChange={this.updateCredentialProperty} />
+                    <p/>
                     <PasswordField value={this.state.editPassword} onChange={this.updateCredentialProperty} />
+                    <p/>
                     {this.state.loading ? <Spinner /> :
-                        <button className="btn btn-lg btn-dark btn-block" type="submit">
+                        <button  className="btn text-light" style={{backgroundColor:'rgb(9,26,78)'}} type="submit">
                             Sign in
                         </button>}
                     <input name="transport_type" type="hidden" value="rest" />
@@ -79,14 +81,14 @@ class Login extends BaseComponent {
 const PasswordField = ({ value, onChange }) => {
     return <Fragment>
         <label className="sr-only">Password</label>
-        <input name="editPassword" value={value} onChange={onChange} type="password" id="inputPassword" className="form-control"
+        <input style={{borderColor:'rgb(9,26,78)'}} name="editPassword" value={value} onChange={onChange} type="password" id="inputPassword" className="form-control"
             placeholder="Password" required />
     </Fragment>
 }
 const UsernameField = ({ value, onChange }) => {
     return (<Fragment>
         <label className="sr-only">Username</label>
-        <input name="username" value={value} onChange={onChange} type="text" id="username" className="form-control"
+        <input style={{borderColor:'rgb(9,26,78)'}} name="username" value={value} onChange={onChange} type="text" id="username" className="form-control"
             placeholder="Username" required autoFocus />
     </Fragment>)
 }
